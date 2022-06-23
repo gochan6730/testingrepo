b6a2300e25c812e8d87ae49d59540a4880bca38a
gochan6730 <102780017+gochan6730@users.noreply.github.com>
2022-06-23 20:22:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+var i;
+i=0; //indicating light is off initially
+function glow(){
+    if(i==0){
+    document.getElementById("light").style.visibility = "visible";
+        i=1; //indicating light is now on
+        document.getElementById("button").style.top="85px";
+        document.getElementById("button").style.boxShadow="none";
+    }
+    else{
+        document.getElementById("light").style.visibility = "hidden";
+        document.getElementById("button").style.top="80px";
+        document.getElementById("button").style.boxShadow="0px 5px #666666";
+        i=0; //indicating light is now off
+    }
+}
+
+//have a console on mobile
+const consoleOutput = document.getElementById("console");
+const log = function (msg) {
+  consoleOutput.innerText = `${consoleOutput.innerText}\n${msg}`;
+  console.log(msg);
+}
+
+//Test browser support
+const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
+
+if (SUPPORTS_MEDIA_DEVICES) {
+  //Get the environment camera (usually the second one)
+  navigator.mediaDevices.enumerateDevices().then(devices => {
+
+    const cameras = devices.filter((device) => device.kind === 'videoinput');
+
+    if (cameras.length === 0) {
+      log('No camera found on this device.');
+    }
+    // Create stream and get video track
+    navigator.mediaDevices.getUserMedia({
+      video: {
+        facingMode: 'environment',
+      }
+    }).then(stream => {
+      const track = stream.getVideoTracks()[0];
+
+      //Create image capture object and get camera capabilities
+      const imageCapture = new ImageCapture(track)
+      imageCapture.getPhotoCapabilities().then(capabilities => {
+        //let there be light!
+        const btn = document.querySelector('.switch');
+        const torchSupported = !!capabilities.torch || (
+          'fillLightMode' in capabilities &&
+          capabilities.fillLightMode.length != 0 &&
+          capabilities.fillLightMode != 'none'
+        );
+
+        if (torchSupported) {
+          let torch = false;
+          btn.addEventListener('click', function (e) {
+            try {
+              track.applyConstraints({
+                advanced: [{
+                  torch: (torch = !torch)
+                }]
+              });
+            } catch (err) {
+              log(err);
+            }
+          });
+        } else {
+          log("No torch found");
+        }
+      }).catch(log);
+    }).catch(log);
+  }).catch(log);
+
+  //The light will be on as long the track exists
+}
