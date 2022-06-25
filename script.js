49b77f354d7f7ea59ee2996bad2f81688a74014b
gochan6730 <102780017+gochan6730@users.noreply.github.com>
2022-06-25 11:17:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,38 @@
-var i;
-i=0; //indicating light is off initially
-function glow(){
-    if(i==0){
-    document.getElementById("light").style.visibility = "visible";
-        i=1; //indicating light is now on
-        document.getElementById("button").style.top="85px";
-        document.getElementById("button").style.boxShadow="none";
-    }
-    else{
-        document.getElementById("light").style.visibility = "hidden";
-        document.getElementById("button").style.top="80px";
-        document.getElementById("button").style.boxShadow="0px 5px #666666";
-        i=0; //indicating light is now off
-    }
+const wrapper = document.querySelector(".wrapper"),
+form = document.querySelector("form"),
+fileInp = form.querySelector("input"),
+infoText = form.querySelector("p"),
+closeBtn = document.querySelector(".close"),
+copyBtn = document.querySelector(".copy");
+
+function fetchRequest(file, formData) {
+    infoText.innerText = "Scanning QR Code...";
+    fetch("http://api.qrserver.com/v1/read-qr-code/", {
+        method: 'POST', body: formData
+    }).then(res => res.json()).then(result => {
+        result = result[0].symbol[0].data;
+        infoText.innerText = result ? "Upload QR Code to Scan" : "Couldn't scan QR Code";
+        if(!result) return;
+        document.querySelector("textarea").innerText = result;
+        form.querySelector("img").src = URL.createObjectURL(file);
+        wrapper.classList.add("active");
+    }).catch(() => {
+        infoText.innerText = "Couldn't scan QR Code";
+    });
 }
 
-//have a console on mobile
-const consoleOutput = document.getElementById("console");
-const log = function (msg) {
-  consoleOutput.innerText = `${consoleOutput.innerText}\n${msg}`;
-  console.log(msg);
-}
+fileInp.addEventListener("change", async e => {
+    let file = e.target.files[0];
+    if(!file) return;
+    let formData = new FormData();
+    formData.append('file', file);
+    fetchRequest(file, formData);
+});
 
-//Test browser support
-const SUPPORTS_MEDIA_DEVICES = 'mediaDevices' in navigator;
+copyBtn.addEventListener("click", () => {
+    let text = document.querySelector("textarea").textContent;
+    navigator.clipboard.writeText(text);
+});
 
-if (SUPPORTS_MEDIA_DEVICES) {
-  //Get the environment camera (usually the second one)
-  navigator.mediaDevices.enumerateDevices().then(devices => {
-
-    const cameras = devices.filter((device) => device.kind === 'videoinput');
-
-    if (cameras.length === 0) {
-      log('No camera found on this device.');
-    }
-    // Create stream and get video track
-    navigator.mediaDevices.getUserMedia({
-      video: {
-        facingMode: 'environment',
-      }
-    }).then(stream => {
-      const track = stream.getVideoTracks()[0];
-
-      //Create image capture object and get camera capabilities
-      const imageCapture = new ImageCapture(track)
-      imageCapture.getPhotoCapabilities().then(capabilities => {
-        //let there be light!
-        const btn = document.querySelector('.switch');
-        const torchSupported = !!capabilities.torch || (
-          'fillLightMode' in capabilities &&
-          capabilities.fillLightMode.length != 0 &&
-          capabilities.fillLightMode != 'none'
-        );
-
-        if (torchSupported) {
-          let torch = false;
-          btn.addEventListener('click', function (e) {
-            try {
-              track.applyConstraints({
-                advanced: [{
-                  torch: (torch = !torch)
-                }]
-              });
-            } catch (err) {
-              log(err);
-            }
-          });
-        } else {
-          log("No torch found");
-        }
-      }).catch(log);
-    }).catch(log);
-  }).catch(log);
-
-  //The light will be on as long the track exists
-}
-
-// Interface
-const checkbox = document.getElementById("checkbox")
-checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark")
-})
+form.addEventListener("click", () => fileInp.click());
+closeBtn.addEventListener("click", () => wrapper.classList.remove("active"));
